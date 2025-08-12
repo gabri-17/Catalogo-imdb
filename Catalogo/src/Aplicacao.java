5a04a92d124c5cd672b8cc843bf8f05733996ab6
Jhon <106191620+LilJhonMoonbeat@users.noreply.github.com>
2025-08-11 21:21:57 -0300

    criação de  objetos

--- Catalogo/src/Aplicacao.java
+++ Catalogo/src/Aplicacao.java
@@ -1,5 +1,105 @@
 public class Aplicacao {
     public static void main(String[] args) {
+        // Instância 1
+        Ator ator1 = new Ator("Janildo", "20/12/1994", "masculino", 123);
+        Diretor diretor1 = new Diretor("Julio", "01/01/2000", "Masculino", 456);
+        Filme filme1 = new Filme("Java para Todos", "20/02/2000", 30000, "Premiadíssimo");
+
+// Instância 2
+        Ator ator2 = new Ator("Jonathan", "15/05/1985", "masculino", 789);
+        Diretor diretor2 = new Diretor("Maria", "10/03/1995", "feminino", 102);
+        Filme filme2 = new Filme("A Hora da Estrela", "15/05/2020", 100000, "Filme bom da gota serena");
+
+// Instância 3
+        Ator ator3 = new Ator("Carla", "10/11/1978", "feminino", 321);
+        Diretor diretor3 = new Diretor("Fernando", "22/07/1970", "masculino", 654);
+        Filme filme3 = new Filme("O Código Oculto", "01/08/2018", 500000, "Suspense intrigante");
+
+// Instância 4
+        Ator ator4 = new Ator("Ricardo", "05/03/1990", "masculino", 987);
+        Diretor diretor4 = new Diretor("Patrícia", "14/09/1988", "feminino", 303);
+        Filme filme4 = new Filme("Missão Impossível", "10/10/2021", 1200000, "Muita ação e aventura");
+
+// Instância 5
+        Ator ator5 = new Ator("Ana", "25/06/1982", "feminino", 234);
+        Diretor diretor5 = new Diretor("Carlos", "19/04/1975", "masculino", 765);
+        Filme filme5 = new Filme("O Silêncio dos Inocentes", "25/11/2015", 750000, "Um clássico do terror");
+
+// Instância 6
+        Ator ator6 = new Ator("Felipe", "30/01/1993", "masculino", 543);
+        Diretor diretor6 = new Diretor("Sofia", "08/02/1991", "feminino", 212);
+        Filme filme6 = new Filme("O Encontro", "05/03/2019", 250000, "Drama emocionante");
+
+// Instância 7
+        Ator ator7 = new Ator("Amanda", "12/04/1987", "feminino", 876);
+        Diretor diretor7 = new Diretor("Daniel", "18/06/1980", "masculino", 898);
+        Filme filme7 = new Filme("Noite Estrelada", "20/09/2022", 900000, "Ficção científica incrível");
+
+// Instância 8
+        Ator ator8 = new Ator("Gustavo", "07/08/1996", "masculino", 111);
+        Diretor diretor8 = new Diretor("Helena", "23/12/1979", "feminino", 432);
+        Filme filme8 = new Filme("Vozes do Passado", "12/07/2016", 400000, "Mistério e suspense");
+
+// Instância 9
+        Ator ator9 = new Ator("Isabela", "18/03/1980", "feminino", 999);
+        Diretor diretor9 = new Diretor("Lucas", "04/05/1986", "masculino", 567);
+        Filme filme9 = new Filme("A Lenda do Dragão", "30/11/2017", 1500000, "Aventura épica");
+
+// Instância 10
+        Ator ator10 = new Ator("Thiago", "29/10/1998", "masculino", 444);
+        Diretor diretor10 = new Diretor("Mariana", "09/01/1994", "feminino", 876);
+        Filme filme10 = new Filme("O Último Suspiro", "14/02/2023", 350000, "Romance dramático");
+
+// Instância 11
+        Ator ator11 = new Ator("Paula", "03/09/1972", "feminino", 222);
+        Diretor diretor11 = new Diretor("Rafael", "17/04/1983", "masculino", 919);
+        Filme filme11 = new Filme("Sob o Sol da Toscana", "22/08/2010", 600000, "Comédia romântica");
+
+// Instância 12
+        Ator ator12 = new Ator("Diego", "21/07/1989", "masculino", 555);
+        Diretor diretor12 = new Diretor("Laura", "28/11/1990", "feminino", 777);
+        Filme filme12 = new Filme("A Cidade dos Espelhos", "03/12/2014", 800000, "Fantasia e magia");
+
+// Instância 13
+        Ator ator13 = new Ator("Viviane", "09/02/1991", "feminino", 777);
+        Diretor diretor13 = new Diretor("Alexandre", "02/06/1977", "masculino", 333);
+        Filme filme13 = new Filme("O Labirinto da Alma", "11/05/2013", 450000, "Suspense psicológico");
+
+// Instância 14
+        Ator ator14 = new Ator("Bruno", "14/12/1983", "masculino", 666);
+        Diretor diretor14 = new Diretor("Carolina", "26/08/1984", "feminino", 222);
+        Filme filme14 = new Filme("Horizonte Perdido", "08/04/2011", 700000, "Ficção de aventura");
+
+// Instância 15
+        Ator ator15 = new Ator("Julia", "27/05/1995", "feminino", 888);
+        Diretor diretor15 = new Diretor("Marcos", "13/03/1976", "masculino", 111);
+        Filme filme15 = new Filme("O Tesouro Perdido", "19/01/2012", 1000000, "Aventura emocionante");
+
+// Instância 16
+        Ator ator16 = new Ator("Daniela", "06/08/1986", "feminino", 333);
+        Diretor diretor16 = new Diretor("Henrique", "20/07/1981", "masculino", 444);
+        Filme filme16 = new Filme("Caminhos Paralelos", "27/09/2018", 300000, "Drama e reflexão");
+
+// Instância 17
+        Ator ator17 = new Ator("Marcelo", "11/03/1992", "masculino", 999);
+        Diretor diretor17 = new Diretor("Fernanda", "05/02/1993", "feminino", 555);
+        Filme filme17 = new Filme("A Última Dança", "02/06/2020", 200000, "Romance e música");
+
+// Instância 18
+        Ator ator18 = new Ator("Leticia", "24/09/1980", "feminino", 112);
+        Diretor diretor18 = new Diretor("Pedro", "16/10/1987", "masculino", 666);
+        Filme filme18 = new Filme("Segredos da Floresta", "13/08/2019", 950000, "Terror e mistério");
+
+// Instância 19
+        Ator ator19 = new Ator("Henrique", "08/01/1979", "masculino", 121);
+        Diretor diretor19 = new Diretor("Silvia", "21/04/1978", "feminino", 888);
+        Filme filme19 = new Filme("Entre Duas Vidas", "28/10/2016", 550000, "Drama intenso");
+
+// Instância 20
+        Ator ator20 = new Ator("Tatiana", "17/11/1997", "feminino", 434);
+        Diretor diretor20 = new Diretor("Renato", "07/12/1982", "masculino", 999);
+        Filme filme20 = new Filme("A Estrada para o Desconhecido", "04/07/2021", 720000, "Ficção científica e aventura");
+        Catalogo catalogo = new Catalogo();
 
     }
 }
